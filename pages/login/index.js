b595494fc0d4c8f60a506b50e68e469d5240cc51
Pika Ship <awaisgohar5432@gmail.com>
2024-03-15 16:42:28 +0500

    login page checkbox and as guest button #12

--- pages/login/index.js
+++ pages/login/index.js
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 import { Breadcrumbs, BreadcrumbItem, Card, Spacer } from "@nextui-org/react";
 import HomeIcon from "@mui/icons-material/Home";
 import loginStyles from "../../styles/loginStyles/login.module.css";
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import Link from "next/link";
+import Button from '@mui/material/Button';
 
 const Index = () => {
   return (
@@ -17,7 +21,7 @@ const Index = () => {
           <BreadcrumbItem>Sign in to your Account</BreadcrumbItem>
         </Breadcrumbs>
       </div>
-      <Spacer y={50} />
+      <Spacer y={5} />
       <div className={loginStyles.card}>
         <div className={loginStyles.loginTitle}>Sign in to your Account</div>
         <div>
@@ -44,12 +48,22 @@ const Index = () => {
                 className={`w-full px-4 py-2 rounded-sm focus:outline-none ${loginStyles.allDetails}`}
               />
             </div>
+            <div className={`flex pb-2 justify-between items-center ${loginStyles.bottomLinks}`}>
+              <FormGroup>
+                <FormControlLabel control={<Checkbox />} label="Remember Me" />
+              </FormGroup>
+              <Link href="/">Lost Your Password?</Link>
+            </div>
             <input
               type="submit"
               value={"LOGIN"}
               className={loginStyles.loginBtn}
             />
           </form>
+          <div className={`text-center pt-2 pb-4 ${loginStyles.allDetails}`}>
+            <p className="py-2">OR</p>
+            <Button variant="contained" className={loginStyles.loginBtn}>CHECK OUT AS A GUEST</Button>
+          </div>
         </div>
       </div>
     </div>
