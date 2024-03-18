3aac74e65417314b2e0d765f7e671b8e5a16c89e
ShahbazAli46 <41480802+ShahbazAli46@users.noreply.github.com>
2024-03-18 11:06:08 +0500

    workin on cart page

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Breadcrumbs, BreadcrumbItem, Spacer } from "@nextui-org/react";
 import HomeIcon from "@mui/icons-material/Home";
-import Link from "next/link";
-import faqStyles from "../../styles/faqs.module.css";
 
 const Index = () => {
   return (
     <div className="container mx-auto">
       <div>
-        <div className={`${faqStyles.titleText}`}>
+        <div className={`${indexStyles.titleText}`}>
           <Breadcrumbs>
             <BreadcrumbItem>
               <Link href="/">{<HomeIcon />}</Link>
             </BreadcrumbItem>
-            <BreadcrumbItem>FAQs</BreadcrumbItem>
+            <BreadcrumbItem>Home</BreadcrumbItem>
+            <BreadcrumbItem>Shop</BreadcrumbItem>
+            <BreadcrumbItem>Product</BreadcrumbItem>
+            <BreadcrumbItem>Cart</BreadcrumbItem>
           </Breadcrumbs>
         </div>
       </div>
