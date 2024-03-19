6fb289828a78d8f456b2dee1a02f5b3af4ed0a1f
ShahbazAli46 <41480802+ShahbazAli46@users.noreply.github.com>
2024-03-19 16:00:46 +0500

    cart page set

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Breadcrumbs, BreadcrumbItem, Spacer } from "@nextui-org/react";
 import HomeIcon from "@mui/icons-material/Home";
 import indexStyles from "../../styles/cartStyles/cartIndesStyles.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 const Index = () => {
-  const carItems = [
+  const [cartItems, setCarItemsArray] = useState([
     {
       itemName: "Signature Hubara Vest",
       itemImage:
         "https://s3.ap-south-1.amazonaws.com/media.hubara.com/upload/inventory/inventory_88263.webp",
       color: "Olive Green",
       size: "Large",
-      price: "3900.00",
+      price: "3,900.00",
       quantity: 1,
     },
     {
@@ -21,7 +22,7 @@ const Index = () => {
         "https://s3.ap-south-1.amazonaws.com/media.hubara.com/upload/inventory/inventory_88263.webp",
       color: "Olive Green",
       size: "Large",
-      price: "3900.00",
+      price: "3,900.00",
       quantity: 1,
     },
     {
@@ -30,28 +31,98 @@ const Index = () => {
         "https://s3.ap-south-1.amazonaws.com/media.hubara.com/upload/inventory/inventory_88263.webp",
       color: "Olive Green",
       size: "Large",
-      price: "3900.00",
+      price: "3,900.00",
       quantity: 1,
     },
-  ];
+  ]);
+
+  useState(() => {}, [cartItems]);
+
+  const deleteItem = (index) => {
+    const updatedCartItems = cartItems.filter((item, i) => i !== index);
+    setCarItemsArray(updatedCartItems);
+  };
+
+  const increaseItem = (index) => {
+    const updatedCartItems = [...cartItems];
+    updatedCartItems[index].quantity += 1;
+    setCarItemsArray(updatedCartItems);
+  };
+  const decreaseBtn = (index) => {
+    const updatedCartItems = [...cartItems];
+    if (updatedCartItems[index].quantity > 1) {
+      updatedCartItems[index].quantity -= 1;
+    }
+    setCarItemsArray(updatedCartItems);
+  };
 
   const setCartItems = () => {
     const cartDivs = [];
-    carItems.forEach((item, index) => {
+    cartItems.forEach((item, index) => {
       const carDiv = (
         <div key={index}>
           <div
-            className={` rounded overflow-hidden shadow-lg mb-5 ${indexStyles.itemParentDiv}`}
+            className={`  overflow-hidden shadow-lg mb-5 ${indexStyles.itemParentDiv}`}
           >
             <div className="flex justify-between">
-              <div className="w-full bg-gray-200 p-4">Content 1</div>
-              <div className="w-full bg-gray-200 p-4">Content 2</div>
-              <div className="w-full bg-gray-200 p-4">Content 3</div>
+              <div className="p-5 w-3/5 mx-auto">
+                <div className="relative">
+                  <Image
+                    src={item.itemImage}
+                    alt={item.title}
+                    width={90}
+                    height={120}
+                    objectFit="cover"
+                  />
+                </div>
+              </div>
+              <div className="w-full p-4">
+                <div className={indexStyles.itemName}> {item.itemName}</div>
+                <div className={indexStyles.colorAndSize}>
+                  Color: {item.color}
+                </div>
+                <div className={`mb-2 ${indexStyles.colorAndSize}`}>
+                  Size: {item.size}
+                </div>
+                <div className={`flex ${indexStyles.qunatityCounterParent}`}>
+                  <div
+                    onClick={() => decreaseBtn(index)}
+                    className={indexStyles.decreaseBtn}
+                  >
+                    -
+                  </div>
+                  <div className={indexStyles.quantityDiv}>{item.quantity}</div>
+                  <div
+                    onClick={() => increaseItem(index)}
+                    className={indexStyles.decreaseBtn}
+                  >
+                    +
+                  </div>
+                </div>
+              </div>
+              <div
+                className={`p-4 w-3/5 ${indexStyles.priceDiv} flex items-center justify-center`}
+              >
+                AED : {item.price}
+              </div>
+
+              <div className={`p-4 w-1/2 relative`}>
+                <div
+                  className={`absolute top-0 right-0 h-full w-30 flex items-center justify-end mr-5 `}
+                >
+                  <Image
+                    className={indexStyles.deleteBtn}
+                    src={"/bin.svg"}
+                    height={15}
+                    width={15}
+                    onClick={() => deleteItem(index)}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
       );
-
       cartDivs.push(carDiv);
     });
     return cartDivs;
@@ -59,10 +130,42 @@ const Index = () => {
 
   const itemsInCart = () => {
     return (
-      <div className="grid grid-cols-12 gap-4">
-        <div className="col-span-12 md:col-span-8 p-4">{setCartItems()}</div>
-        <div className="col-span-12 md:col-span-4 p-4">
-          Item 2 (6 columns on desktop)
+      <div className="grid grid-cols-12 gap-1">
+        <div className="col-span-12 md:col-span-8 p-4">
+          <div>{setCartItems()}</div>
+        </div>
+        <div className="col-span-12 md:col-span-4">
+          <div className={indexStyles.summaryTex}>Summary</div>
+          <div
+            className={`${indexStyles.summaryParent} w-full overflow-hidden shadow-lg`}
+          >
+            <div className="flex">
+              <div className={`flex-grow ${indexStyles.summaryItems}`}>
+                SubTotal
+              </div>
+              <div className={`flex-shrink ${indexStyles.summaryItems}`}>
+                AED 7,800.00
+              </div>
+            </div>
+            <div className="flex">
+              <div className={`flex-grow ${indexStyles.summaryItems}`}>
+                Delivery Fee
+              </div>
+              <div className={`flex-shrink ${indexStyles.summaryItems}`}>
+                AED 00.00
+              </div>
+            </div>
+
+            <div className="flex">
+              <div className={`flex-grow ${indexStyles.summaryItems}`}>
+                Total
+              </div>
+              <div className={`flex-shrink ${indexStyles.summaryItems}`}>
+                AED 7,800.00
+              </div>
+            </div>
+          </div>
+          <div className={indexStyles.proceedBtn}>Proceed to Shipment</div>
         </div>
       </div>
     );
